a60ff2d055b614f081d113408124f6cdec44e4a5
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-03-08 16:04:50 -0500

    moved come from static page to a nav comp, auth comp, and user module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserModule } from './user/user.module';
+import { NavComponent } from "./nav/nav.component";
+import { AuthModalComponent } from './user/auth-modal/auth-modal.component';
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, UserModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, UserModule, NavComponent,AuthModalComponent]
 })
 export class AppComponent {
   title = 'clips';
---
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-modal.component.html',
+  styleUrl: './auth-modal.component.css'
+})
+export class AuthModalComponent {
+
+}
